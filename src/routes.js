5de8bc7f2c0feeb8e7948bfe5e56c18876560dfa
Beto Muniz <contato@betomuniz.com>
2017-04-24 18:59:55 -0300

    Improving the routes code

--- src/routes.js
+++ src/routes.js
@@ -4,25 +4,14 @@ import Switch from 'react-router-dom/Switch';
 import { Layout } from '~components';
 import { Home, About, NotFound } from '~pages';
 
-export const routes = [
-  {
-    component: Layout,
-    routes: [
-      {
-        path: '/',
-        exact: true,
-        component: Home
-      },
-      {
-        path: '/about',
-        component: About
-      },
-      {
-        component: NotFound
-      }
-    ]
-  }
-];
+export const routes = [{
+  component: Layout,
+  routes: [
+    { path: '/', exact: true, component: Home },
+    { path: '/about', component: About },
+    { component: NotFound }
+  ]
+}];
 
 export const renderRoutes = (routesList) => {
   const list = routesList ? (
