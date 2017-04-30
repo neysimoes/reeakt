c414ad137ac6be55d9bce490716655601333c4a0
Beto Muniz <contato@betomuniz.com>
2017-04-30 05:04:47 -0300

    Update redux-saga code requirements

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,10 +1,10 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, all } from 'redux-saga/effects';
 import { FETCH_TESTS_REQUEST } from '~reducers/Tests/types';
 import { fetchTests } from '~sagas/Tests';
 
 /* eslint-disable redux-saga/no-unhandled-errors */
 export default function* rootSaga() {
-  yield [
+  yield all([
     takeEvery(FETCH_TESTS_REQUEST, fetchTests)
-  ];
+  ]);
 }
