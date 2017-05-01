bf754b7b08db1117fc22584cc26f5561a4e2b078
Beto Muniz <contato@betomuniz.com>
2017-05-01 12:49:58 -0300

    Update tests

--- src/components/Html/__tests__/index.test.js
+++ src/components/Html/__tests__/index.test.js
@@ -7,7 +7,15 @@ import Html from '~components/Html';
 describe('<Html />', () => {
   it('should render an <Html> tag', () => {
     Helmet.canUseDOM = false;
-    const renderedComponent = shallow(<Html component={<div />} assets={{ javascript: {main: '', vendor: '' }}} store={{ getState: () => true }} />);
+    const renderedComponent = shallow(<Html component={<div />} assets={{ javascript: {main: '', vendor: '' }, styles: { main: '' }}} store={{ getState: () => true }} />);
+    expect(renderedComponent.type()).toEqual('html');
+  });
+});
+
+describe('<Html />', () => {
+  it('should render an <Html> tag', () => {
+    Helmet.canUseDOM = false;
+    const renderedComponent = shallow(<Html assets={{ javascript: {main: '', vendor: '' }, styles: { main: '' }}} store={{ getState: () => true }} />);
     expect(renderedComponent.type()).toEqual('html');
   });
 });
